5e7f3c05049cb7abd59ccabba7982b0a61452399
Bruno <brunosmmartins@gmail.com>
2020-12-19 13:05:09 -0300

    Rollback modification
     The reverse should be made in the migrations array and not the collections

--- src/getAppliedMigrations.ts
+++ src/getAppliedMigrations.ts
@@ -2,12 +2,10 @@ import { Client, query as q } from "faunadb";
 
 const getAppliedMigrations = (client: Client) =>
   client.query(
-    q.Reverse(
         q.Map(
           q.Paginate(q.Match(q.Index("all_migrations"))),
           q.Lambda("ref", q.Get(q.Var("ref")))
         )
-    )
   );
 
 export default getAppliedMigrations;
