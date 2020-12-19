c986ebbc05d46e9628e4ed304748e15bcae44716
Bruno <brunosmmartins@gmail.com>
2020-12-19 12:21:17 -0300

    Fix issue with rollback order
    Should start by the latest applied to guarantee no conflict

--- dist/getAppliedMigrations.js
+++ dist/getAppliedMigrations.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var faunadb_1 = require("faunadb");
 var getAppliedMigrations = function (client) {
-    return client.query(faunadb_1.query.Map(faunadb_1.query.Paginate(faunadb_1.query.Match(faunadb_1.query.Index("all_migrations"))), faunadb_1.query.Lambda("ref", faunadb_1.query.Get(faunadb_1.query.Var("ref")))));
+    return client.query(faunadb_1.query.Reverse(faunadb_1.query.Map(faunadb_1.query.Paginate(faunadb_1.query.Match(faunadb_1.query.Index("all_migrations"))), faunadb_1.query.Lambda("ref", faunadb_1.query.Get(faunadb_1.query.Var("ref"))))));
 };
 exports.default = getAppliedMigrations;
